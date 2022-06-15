a9038642d3b4f80289dc42a4bbfed7aae296c145
Alex Remizov <rem.remizov@yandex.ru>
2022-06-15 13:43:39 -0400

    Initial commit

---
+++ src/test/java/com/remizov/springboot/users/UsersApplicationTests.java
@@ -0,0 +1,13 @@
+package com.remizov.springboot.users;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UsersApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
